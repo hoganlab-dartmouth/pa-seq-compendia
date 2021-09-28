f2bc488e94fa041513daafa9df3c1a4d63a15595
Georgia <georgia@Georgias-MacBook-Pro-3.local>
2021-09-28 11:04:49 -0400

    updated filtering code and included helper functions

--- qc_filtering/filter_functions.R
+++ qc_filtering/filter_functions.R
@@ -21,24 +21,42 @@
 library(RVenn)
 source('~/Dropbox (Hogan Lab)/Resources/Scripts/fsqn.R')
 
-array_match <- function(a_comp, s_comp, a_ref=T, fsqn=T){
-  dfs <- list(a_comp, s_comp)
-  genes <- lapply(dfs, function(x) rownames(x))
-  genes_venn <- Venn(genes)
-  common_genes <- overlap(genes_venn)
-  if(fsqn){
-    if(a_ref){
-      s_comp_fsqn <- quantileNormalizeByFeature(t(s_comp[common_genes,]),t(a_comp[common_genes,]))
-      comp_bind <- cbind(a_comp[common_genes,],t(s_comp_fsqn))
-    } else{
-      a_comp_fsqn <- quantileNormalizeByFeature(t(a_comp[common_genes,]),t(s_comp[common_genes,]))
-      comp_bind <- cbind(t(a_comp_fsqn),s_comp[common_genes,])
-    }
-  }else{
-    comp_bind <- cbind(a_comp[common_genes,],s_comp[common_genes,])
-  }
+hlog <- function(df, base=exp(1)){
+  # df: dataframe of gene expression to be logged
+  # base: base for log calculation
+  # returns: log of df with happy 0s for 0 or neg values
+  dfl <- data.frame(log(data.matrix(df),base))
+  dfl[dfl == -Inf] <- 0
+  dfl[dfl < 0] <- 0
+  dfl
+}
 
-  return(comp_bind)
+MRnorm <- function(data){
+  # data: dataframe of gene expression counts to be normalized via DESeq2
+  meta <- data.frame(experiment = colnames(data))
+  dds <- DESeqDataSetFromMatrix(countData = 
+                                       ceiling(data), 
+                                     colData = meta, design = ~ experiment)
+  dds <- estimateSizeFactors(dds)
+  counts(dds, normalized=TRUE)
+}
+
+get_zeros <- function(data, lt= .1, ut = .9){
+  # data:
+  # lt:
+  # ut:
+  quantile(apply(
+    data, 2, 
+    FUN = function(x) sum( x  == 0)), probs = c(lt,ut))
+}
+
+get_hks <- function(data, hk_genes, lt=.2,ut=.98){
+  # data:
+  # hk_genes:
+  # lt: 
+  # ut:
+  quantile(apply(data[hks_genes,], 
+                 2, FUN = function(x) median(x)), probs = c(lt,ut))
 }
 
 filter_hks <- function(data, hk_genes=NA, hk_min=4, hk_max=6){
@@ -57,15 +75,15 @@ filter_hks <- function(data, hk_genes=NA, hk_min=4, hk_max=6){
   } else{
     hk_genes <- hk_genes[hk_clean]
   }
-  hk_means <- apply(data[hk_genes,], 2, FUN = function(x) mean(x))
+  hk_means <- apply(data[hk_genes,], 2, FUN = function(x) median(x))
   hk_filt <- (hk_means >= hk_min) & (hk_means <= hk_max)
   return(hk_filt)
 }
 
-filter_sparsity <- function(data, max_zeros=1000, min_zeros = 200, min_peak=0.15){
+filter_sparsity <- function(data, max_zeros=1000, min_zeros = 200, min_peak=0){
   # data: dataframe of gene expression with rownames as gene names
   # max_zeros: int of maximum unexpressed gene per sample, default infinity
-  # min_peak: float [0-1] of multiplier for peak cutoff, default 1
+  # min_peak: float [0-1] of multiplier for peak cutoff, default 1 (0.15 good)
   # returns: array of bools for filtering columns of data
   if(max_zeros < 0 | min_peak < 0){
     print('numer of zeros and peak coefficinet must be positive float values')
@@ -79,7 +97,28 @@ filter_sparsity <- function(data, max_zeros=1000, min_zeros = 200, min_peak=0.15
       F
     } else {T}
   })
-  sparse_filt <- toosp_test & unsparse_test_p & unsparse_test_z
+  sparse_filt <- toosp_test & unsparse_test_z #& unsparse_test_p 
   return(sparse_filt)
 }
 
+
+
+array_match <- function(a_comp, s_comp, a_ref=T, fsqn=T){
+  dfs <- list(a_comp, s_comp)
+  genes <- lapply(dfs, function(x) rownames(x))
+  genes_venn <- Venn(genes)
+  common_genes <- overlap(genes_venn)
+  if(fsqn){
+    if(a_ref){
+      s_comp_fsqn <- quantileNormalizeByFeature(t(s_comp[common_genes,]),t(a_comp[common_genes,]))
+      comp_bind <- cbind(a_comp[common_genes,],t(s_comp_fsqn))
+    } else{
+      a_comp_fsqn <- quantileNormalizeByFeature(t(a_comp[common_genes,]),t(s_comp[common_genes,]))
+      comp_bind <- cbind(t(a_comp_fsqn),s_comp[common_genes,])
+    }
+  }else{
+    comp_bind <- cbind(a_comp[common_genes,],s_comp[common_genes,])
+  }
+  
+  return(comp_bind)
+}
